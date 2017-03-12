26918b4c5324c79ae13785f9a8a31a6d649ac91d
Julian <Juliancl1997@gmail.com>
2017-03-11 20:35:57 -0500

    Login

---
+++ Lab2Activities/src/main/java/co/edu/udea/compumovil/gr09_20171/lab2/AdminSQLiteOpenHelper.java
@@ -0,0 +1,27 @@
+package co.edu.udea.compumovil.gr09_20171.lab2;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Julian on 10/03/2017.
+ */
+
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+    public AdminSQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table users(user text primary key not null,email text not null,password text not null)");
+
+        //db.execSQL("create table events(id int primary key autoincrement,photo text , name text not null,descripcion text, puntuacion double");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
--- Lab2Activities/src/main/java/co/edu/udea/compumovil/gr09_20171/lab2/Login.java
+++ Lab2Activities/src/main/java/co/edu/udea/compumovil/gr09_20171/lab2/Login.java
@@ -1,13 +1,85 @@
 package co.edu.udea.compumovil.gr09_20171.lab2;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Login extends AppCompatActivity implements View.OnClickListener {
+    private Button login;
+    private Button registrar;
+    private EditText user;
+    private EditText pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login = (Button) findViewById(R.id.bttLogin);
+        registrar = (Button) findViewById(R.id.bttReg);
+        user = (EditText) findViewById(R.id.edUserL);
+        pass = (EditText) findViewById(R.id.edPassL);
+        login.setOnClickListener(this);
+        registrar.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.bttLogin:
+                log();
+                break;
+            case R.id.bttReg:
+                break;
+        }
+    }
+
+    private void log() {
+        String usuario = user.getText().toString();
+        String password = pass.getText().toString();
+        if (validar(usuario, password)) {
+            TimerTask task = new TimerTask() {
+                @Override
+                public void run() {
+                    //iniciar siguiente actividad
+                    /*Intent mainIntent = new Intent().setClass(
+                            Login.this, Test.class);
+                    startActivity(mainIntent);*/
+                }
+            };
+            Timer timer = new Timer();
+            //tiempo en decimas
+            timer.schedule(task, 3000);
+        }
+    }
+
+    private boolean validar(String u, String p) {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,
+                "compumovil", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();
+        Cursor fila = bd.rawQuery("select password from users where user=\"" + u + "\"", null);
+        if (fila.moveToFirst()) {
+            if (fila.getString(0).equals(p)) {
+                Toast.makeText(this, getString(R.string.LoginOk) + u,
+                        Toast.LENGTH_SHORT).show();
+                return true;
+            } else {
+                Toast.makeText(this, R.string.ErrorLogin, Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, R.string.ErrorLogin, Toast.LENGTH_SHORT).show();
+        }
+
+        bd.close();
+        return false;
     }
 }
+
