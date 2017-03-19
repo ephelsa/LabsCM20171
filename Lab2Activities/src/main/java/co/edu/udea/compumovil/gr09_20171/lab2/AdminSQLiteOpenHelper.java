7245da157ebb738233db049c858b067fab8b098f
Julian <Juliancl1997@gmail.com>
2017-03-18 20:49:10 -0500

    cambio bd

--- Lab2Activities/src/main/java/co/edu/udea/compumovil/gr09_20171/lab2/AdminSQLiteOpenHelper.java
+++ Lab2Activities/src/main/java/co/edu/udea/compumovil/gr09_20171/lab2/AdminSQLiteOpenHelper.java
@@ -1,6 +1,7 @@
 package co.edu.udea.compumovil.gr09_20171.lab2;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -19,11 +20,12 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
                 "(user text primary key not null," +
                 "email text not null," +
                 "password text not null," +
-                "photo text," +
-                "edad text)");
+                "photo BLOB," +
+                "edad text" +
+                ")");
         db.execSQL("create table events" +
                 "(id INTEGER primary key autoincrement," +
-                "photo text," +
+                "photo BLOB," +
                 "name text not null," +
                 "descripcion text," +
                 "puntuacion float," +
@@ -36,4 +38,119 @@ public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
+
+    protected String getEmailUser(String u){
+        String res="";
+        SQLiteDatabase bd = this.getWritableDatabase();
+        Cursor fila = bd.rawQuery("select email from users where user=\"" + u + "\"", null);
+        if (fila.moveToFirst()) {
+            res=fila.getString(0);
+        }
+        return res;
+    }
+
+    protected String getEdadUser(String u,int dat){
+        String res="";
+        SQLiteDatabase bd = this.getWritableDatabase();
+        Cursor fila = bd.rawQuery("select edad from users where user=\"" + u + "\"", null);
+        if (fila.moveToFirst()) {
+            res=fila.getString(0);
+        }
+        return res;
+    }
+
+    protected byte[] getFotoUser(String u){
+        byte []res;
+        SQLiteDatabase bd = this.getWritableDatabase();
+        Cursor fila = bd.rawQuery("select foto from users where user=\"" + u + "\"", null);
+        if (fila.moveToFirst()) {
+            res=fila.getBlob(0);
+        }else{
+            res=null;
+        }
+        return res;
+
+    }
+
+    protected String getPassUser(String u){
+        String res="";
+        SQLiteDatabase bd = this.getWritableDatabase();
+        Cursor fila = bd.rawQuery("select password from users where user=\"" + u + "\"", null);
+        if (fila.moveToFirst()) {
+            res=fila.getString(0);
+        }
+        return res;
+    }
+
+    protected byte[] getFotoEvent(int id){
+        byte[] res;
+        SQLiteDatabase bd = this.getWritableDatabase();
+        Cursor fila = bd.rawQuery("select photo from events where id=" + id, null);
+        if (fila.moveToFirst()) {
+            res=fila.getBlob(0);
+        }else res=null;
+        return res;
+    }
+
+    protected String getNameEvent(int id){
+        String res="";
+        SQLiteDatabase bd=this.getWritableDatabase();
+        Cursor fila=bd.rawQuery("select name from events where id="+id,null);
+        if(fila.moveToFirst()){
+            res=fila.getString(0);
+        }
+        return res;
+    }
+
+    protected String getDescripcionEvent(int id){
+        String res="";
+        SQLiteDatabase bd=this.getWritableDatabase();
+        Cursor fila=bd.rawQuery("select descripcion from events where id="+id,null);
+        if(fila.moveToFirst()){
+            res=fila.getString(0);
+        }
+        return res;
+    }
+
+    protected float getPuntuacionEvent(int id){
+        float res;
+        SQLiteDatabase bd=this.getWritableDatabase();
+        Cursor fila=bd.rawQuery("select puntuacion from events where id="+id,null);
+        if(fila.moveToFirst()){
+            res=fila.getFloat(0);
+        }else res=-1;
+        return res;
+    }
+
+    protected String getResponsableEvent(int id){
+        String res="";
+        SQLiteDatabase bd=this.getWritableDatabase();
+        Cursor fila=bd.rawQuery("select responsable from events where id="+id,null);
+        if(fila.moveToFirst()){
+            res=fila.getString(0);
+        }
+        return res;
+    }
+
+    protected String getFechaEvent(int id){
+        String res="";
+        SQLiteDatabase bd=this.getWritableDatabase();
+        Cursor fila=bd.rawQuery("select fecha, ubicacion from events where id="+id,null);
+        if(fila.moveToFirst()){
+            res=fila.getString(0);
+        }
+        return res;
+    }
+
+    protected String getUbicacionEvent(int id){
+        String res="";
+        SQLiteDatabase bd=this.getWritableDatabase();
+        Cursor fila=bd.rawQuery("select ubicacion from events where id="+id,null);
+        if(fila.moveToFirst()){
+            res=fila.getString(0);
+        }
+        return res;
+    }
+
+
 }
