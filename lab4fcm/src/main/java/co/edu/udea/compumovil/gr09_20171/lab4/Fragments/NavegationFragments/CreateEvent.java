fb56dd5de22ef1e475cef050f19817a20ccf6781
Julian <juliancl1997@gmail.com>
2017-05-14 15:42:33 -0500

    14/05/2017 15:42

--- lab4fcm/src/main/java/co/edu/udea/compumovil/gr09_20171/lab4/Fragments/NavegationFragments/CreateEvent.java
+++ lab4fcm/src/main/java/co/edu/udea/compumovil/gr09_20171/lab4/Fragments/NavegationFragments/CreateEvent.java
@@ -1,5 +1,9 @@
 package co.edu.udea.compumovil.gr09_20171.lab4.Fragments.NavegationFragments;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -18,16 +22,22 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 
+import java.io.IOException;
+
 import co.edu.udea.compumovil.gr09_20171.lab4.Model.PostEvent;
 import co.edu.udea.compumovil.gr09_20171.lab4.R;
 
-public class CreateEvent extends Fragment implements View.OnClickListener {
-    final public static String EVENTS_REFERENCE = "Events";
-    ImageView eventphoto;
-    EditText titlev, fechav, locationv, information;
-    RatingBar score;
-    Button create;
+import static android.app.Activity.RESULT_OK;
 
+public class CreateEvent extends Fragment implements View.OnClickListener {
+    final private static String EVENTS_REFERENCE = "Events";
+    final private static int PHOTO_SELECTED=2;
+    private ImageView eventphoto;
+    private EditText titlev, fechav, locationv, information;
+    private RatingBar score;
+    private Button create;
+
+    private Bitmap img;
 
     public CreateEvent() {
 
@@ -64,6 +74,9 @@ public class CreateEvent extends Fragment implements View.OnClickListener {
             case R.id.CEFecha:
                 break;
             case R.id.CEImagen:
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, PHOTO_SELECTED);
                 break;
             case R.id.CEBtnPost:
                 CrearEvento(titlev.getText().toString(),
@@ -82,20 +95,42 @@ public class CreateEvent extends Fragment implements View.OnClickListener {
         final FirebaseUser user = firebaseAuth.getCurrentUser();
         PostEvent event = new PostEvent(user.getUid(), user.getDisplayName(), title, fecha, location, info, photo);
 
-        if(eventReference.push().setValue(event).isSuccessful()) {
-            Toast.makeText(this.getContext(),"Evento creado",Toast.LENGTH_SHORT).show();
-        };
-
+        if (eventReference.push().setValue(event).isSuccessful()) {
+            Toast.makeText(this.getContext(), "Evento creado", Toast.LENGTH_SHORT).show();
+        }
+        ;
 
 
     }
 
-    private void subirImagen(){
+    private void subirImagen() {
 
-        FirebaseStorage storage=FirebaseStorage.getInstance();
+        FirebaseStorage storage = FirebaseStorage.getInstance();
         StorageReference storageRef = storage.getReferenceFromUrl("gs://lab4fcm-fc89d.appspot.com/ImgEvents");
 
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PHOTO_SELECTED) {
+            if (resultCode == RESULT_OK) {
+                Uri selectedImage = data.getData();
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), selectedImage);
+                    bitmap = Bitmap.createScaledBitmap(bitmap, 100, 100, false);
+                    img = bitmap;
+                    eventphoto.setImageBitmap(bitmap);
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }
+    }
 
 }
---
+++ lab4fcm/src/main/java/co/edu/udea/compumovil/gr09_20171/lab4/Fragments/NavegationFragments/UpdatePerfilActivity.java
@@ -0,0 +1,15 @@
+package co.edu.udea.compumovil.gr09_20171.lab4.Fragments.NavegationFragments;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import co.edu.udea.compumovil.gr09_20171.lab4.R;
+
+public class UpdatePerfilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_perfil);
+    }
+}
