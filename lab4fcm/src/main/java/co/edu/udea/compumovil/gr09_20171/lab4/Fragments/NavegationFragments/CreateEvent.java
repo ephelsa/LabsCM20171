e1b3daf9b2b5f211fc36acebf2103b55fdbd0cba
Julian <juliancl1997@gmail.com>
2017-05-14 20:16:47 -0500

    14/05/2017 20:16

--- lab4fcm/src/main/java/co/edu/udea/compumovil/gr09_20171/lab4/Fragments/NavegationFragments/CreateEvent.java
+++ lab4fcm/src/main/java/co/edu/udea/compumovil/gr09_20171/lab4/Fragments/NavegationFragments/CreateEvent.java
@@ -1,11 +1,16 @@
 package co.edu.udea.compumovil.gr09_20171.lab4.Fragments.NavegationFragments;
 
+import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,14 +20,19 @@ import android.widget.ImageView;
 import android.widget.RatingBar;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
+import java.util.UUID;
 
 import co.edu.udea.compumovil.gr09_20171.lab4.Model.PostEvent;
 import co.edu.udea.compumovil.gr09_20171.lab4.R;
@@ -31,13 +41,15 @@ import static android.app.Activity.RESULT_OK;
 
 public class CreateEvent extends Fragment implements View.OnClickListener {
     final private static String EVENTS_REFERENCE = "Events";
-    final private static int PHOTO_SELECTED=2;
+    final private static int PHOTO_SELECTED = 2;
     private ImageView eventphoto;
     private EditText titlev, fechav, locationv, information;
     private RatingBar score;
     private Button create;
-
+    private Uri selectedImage;
+    private String Uid;
     private Bitmap img;
+    private Fragment eventView;
 
     public CreateEvent() {
 
@@ -75,15 +87,17 @@ public class CreateEvent extends Fragment implements View.OnClickListener {
                 break;
             case R.id.CEImagen:
                 Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("image/*");
+                intent.setType("image/jpeg");
                 startActivityForResult(intent, PHOTO_SELECTED);
                 break;
             case R.id.CEBtnPost:
-                CrearEvento(titlev.getText().toString(),
+                subirImagen(UUID.randomUUID().toString());
+               /* CrearEvento(titlev.getText().toString(),
                         fechav.getText().toString(),
                         locationv.getText().toString(),
                         information.getText().toString(),
-                        "https://firebasestorage.googleapis.com/v0/b/lab4fcm-fc89d.appspot.com/o/ImgEvents%2FDEFAULT.jpg?alt=media&token=81462715-1cec-4f59-b257-428f32ab72e1");
+                        UUID.randomUUID().toString());
+                       */ //"https://firebasestorage.googleapis.com/v0/b/lab4fcm-fc89d.appspot.com/o/ImgEvents%2FDEFAULT.jpg?alt=media&token=81462715-1cec-4f59-b257-428f32ab72e1");
                 break;
         }
     }
@@ -95,18 +109,51 @@ public class CreateEvent extends Fragment implements View.OnClickListener {
         final FirebaseUser user = firebaseAuth.getCurrentUser();
         PostEvent event = new PostEvent(user.getUid(), user.getDisplayName(), title, fecha, location, info, photo);
 
-        if (eventReference.push().setValue(event).isSuccessful()) {
+        boolean result = eventReference.push().setValue(event).isComplete();
+        toEventView();
+        if (result) {
             Toast.makeText(this.getContext(), "Evento creado", Toast.LENGTH_SHORT).show();
         }
-        ;
 
 
     }
 
-    private void subirImagen() {
+    @SuppressWarnings("VisibleForTests")
+    private void subirImagen(final String child) {
+        if (isNetDisponible()) {
+            if (selectedImage != null) {
+                FirebaseStorage storage = FirebaseStorage.getInstance();
+                StorageReference storageRef = storage.getReferenceFromUrl("gs://lab4fcm-fc89d.appspot.com/");
+                StorageReference upImg = storageRef.child("ImgEvents/" + child + ".jpg");
+                UploadTask uploadTask = upImg.putFile(selectedImage);
+                uploadTask.addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
 
-        FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageRef = storage.getReferenceFromUrl("gs://lab4fcm-fc89d.appspot.com/ImgEvents");
+                    }
+                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                        CrearEvento(titlev.getText().toString(),
+                                fechav.getText().toString(),
+                                locationv.getText().toString(),
+                                information.getText().toString(),
+                                taskSnapshot.getDownloadUrl().toString());
+                    }
+                });
+            } else {
+                CrearEvento(titlev.getText().toString(),
+                        fechav.getText().toString(),
+                        locationv.getText().toString(),
+                        information.getText().toString(),
+                        "https://firebasestorage.googleapis.com/v0/b/lab4fcm-fc89d.appspot.com/o/ImgEvents%2FDEFAULT.jpg?alt=media&token=81462715-1cec-4f59-b257-428f32ab72e1");
+            }
+        } else {
+            Toast.makeText(this.getContext(), "Sin internet", Toast.LENGTH_SHORT).show();
+            toEventView();
+        }
+
 
     }
 
@@ -116,21 +163,24 @@ public class CreateEvent extends Fragment implements View.OnClickListener {
 
         if (requestCode == PHOTO_SELECTED) {
             if (resultCode == RESULT_OK) {
-                Uri selectedImage = data.getData();
-                try {
-                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), selectedImage);
-                    bitmap = Bitmap.createScaledBitmap(bitmap, 100, 100, false);
-                    img = bitmap;
-                    eventphoto.setImageBitmap(bitmap);
 
-
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                selectedImage = data.getData();
+                Glide.with(this).load(selectedImage).into(eventphoto);
 
 
             }
         }
     }
 
+    private boolean isNetDisponible() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return networkInfo != null && networkInfo.isAvailable() && networkInfo.isConnected();
+    }
+
+    private void toEventView() {
+        FragmentManager fragmentManager = getFragmentManager();
+        eventView = new EventsViewActivity();
+        fragmentManager.beginTransaction().replace(R.id.fragment_content, eventView).commit();
+    }
 }
--- lab4fcm/src/main/java/co/edu/udea/compumovil/gr09_20171/lab4/Fragments/NavegationFragments/UpdatePerfilActivity.java
+++ lab4fcm/src/main/java/co/edu/udea/compumovil/gr09_20171/lab4/Fragments/NavegationFragments/UpdatePerfilActivity.java
@@ -1,15 +1,54 @@
 package co.edu.udea.compumovil.gr09_20171.lab4.Fragments.NavegationFragments;
 
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import co.edu.udea.compumovil.gr09_20171.lab4.R;
 
-public class UpdatePerfilActivity extends AppCompatActivity {
+public class UpdatePerfilActivity extends Fragment implements View.OnClickListener {
+    private EditText username, email, age;
+    private FirebaseAuth firebaseAuth;
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+      firebaseAuth = FirebaseAuth.getInstance();
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener(){
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+            }};
+        FirebaseUser user=firebaseAuth.getCurrentUser();
+
+
+            View view = inflater.inflate(R.layout.activity_update_perfil, container, false);
+        username = (EditText) view.findViewById(R.id.Upd_perfil_User);
+        email = (EditText) view.findViewById(R.id.Upd_perfil_email);
+        age = (EditText) view.findViewById(R.id.Upd_perfil_edad);
+        Button actualizar = (Button) view.findViewById(R.id.Upd_perfil_act);
+        username.setText(user.getDisplayName());
+        email.setText(user.getEmail());
+        age.setText("");
+        actualizar.setOnClickListener(this);
+        return view;
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_update_perfil);
+    public void onClick(View v) {
+
     }
 }
